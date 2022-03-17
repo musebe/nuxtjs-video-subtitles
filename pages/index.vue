aaa7eb94b7a93ebc5098e91457548d69eed3def6
musebe <emusebe9@gmail.com>
2022-03-17 15:06:33 +0300

    Subtitle display complete

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <cld-video :publicId="video" controls="true" autoplay="true" >
+    <cld-transformation :overlay="{publicId: subtitle, resourceType: 'subtitles'}" />
+    <cld-transformation flags="layer_apply" />
+  </cld-video>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      video:"nuxtjs-video-subtitles/video",
+      subtitle:"nuxtjs-video-subtitles/subtitles.srt",
+    }
+  }
+}
+</script>
